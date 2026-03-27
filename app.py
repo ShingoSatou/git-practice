95f2ec0541b9395f5adada7ad80ba3b895860c6d
ShingoSatou <shingo.sato.1022@gmail.com>
2026-03-28 08:47:50 +0900

    feat: mainブロックにdivideの使用例を追加

--- app.py
+++ app.py
@@ -34,3 +34,4 @@ if __name__ == "__main__":
     print(f"1 + 2 = {add(1, 2)}")
     print(f"3 × 4 = {multiply(3, 4)}")
     print(f"5 - 3 = {subtract(5, 3)}")
+    print(f"10 ÷ 3 = {divide(10, 3)}")
